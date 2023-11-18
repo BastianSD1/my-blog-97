6ae9ca098c5b340b334f9e164fa2c15cf03cc599
BastianSD1 <139516022+BastianSD1@users.noreply.github.com>
2023-11-18 09:48:28 -0600

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+// Crea la función addUser()
+function addUser() {
+   id= player1_name_input id  player2_name_input
+
+  console.log=player1_name_input
+
+ console.log="game_page.html" . window.location
+}
+
